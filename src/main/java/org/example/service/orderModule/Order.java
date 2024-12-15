df0bd34d03cc6a7e6a030757b7b14ea3ae4ba300
Rhuan Nascimento Ferreira <rhuan2414@gmail.com>
2024-12-15 18:38:49 -0300

    REFAC: UPDATE DIAGRAM

--- src/main/java/org/example/service/orderModule/Order.java
+++ src/main/java/org/example/service/orderModule/Order.java
@@ -4,8 +4,22 @@ import org.example.service.paymentModule.PaymentMethod;
 
 public abstract class Order {
     private PaymentMethod paymentMethod;
+    private double value;
 
     public Order(PaymentMethod paymentMethod){
         this.paymentMethod = paymentMethod;
+    }
+
+    public String finish() throws Exception{
+        try{
+            Boolean result = paymentMethod.pay(value);
+            if(result){
+                return "Sucesso";
+            }else{
+                return "Recusado";
+            }
+        }catch (Exception e){
+            throw e;
+        }
     }
 }
